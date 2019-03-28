0b54a1884c24199e4af14b0ceafee696f50a2476
Pierce Irvin <pierce.irvin@workstate.com>
2019-03-28 10:54:44 -0400

    Progress is being made

--- scripts/netsheet.js
+++ scripts/netsheet.js
@@ -1,4 +1,58 @@
-var doc = new jsPDF()
+$(document).ready(function(){
+    $('#btnCalculate').click(function() {
+       const saleTotal = parseInt($('#inputSalePrice').val());
+       const totalExpenses = calculateExpenses(saleTotal);
+       const totalEquity = saleTotal - totalExpenses;
 
-doc.text('Hello world!', 10, 10)
-doc.save('a4.pdf')
+       $('#inputTotalExpenses').val(totalExpenses);
+       $('#inputSellerEquity').val(totalEquity);
+    });
+
+    $('#inputSalePrice').keyup(function() {
+        updatePriceAndCommissions();
+    });
+
+    $('#inputSaleRate').keyup(function() {
+        updatePriceAndCommissions();
+    });
+
+    $('#inputListRate').keyup(function() {
+        updatePriceAndCommissions();
+    });
+
+    function updatePriceAndCommissions() {
+        
+    }
+
+    function calculateExpenses(saleTotal) {
+        const taxTotal = calculateTaxes(saleTotal);
+        const feeTotal = calculateFees();
+
+        return taxTotal + feeTotal;
+    }
+
+    function calculateTaxes(saleTotal) {
+        return 0;
+    }
+
+    function calculateFees() {
+        const firstMortgageBalance = $('#inputMortFirst').val() ? parseInt($('#inputMortFirst').val()) : 0;
+        const secondMortgageBalance = $('#inputMortSecond').val() ? parseInt($('#inputMortSecond').val()) : 0;
+        const mortgageTotal = firstMortgageBalance + secondMortgageBalance;
+
+        const transferFees = $('#inputTransferFees').val() ? parseInt($('#inputTransferFees').val()) : 0;
+
+        const saleCommission = $('#inputSaleCommTotal').val() ? parseInt($('#inputSaleCommTotal').val()) : 0;
+        const listCommission = $('#inputListCommTotal').val() ? parseInt($('#inputListCommTotal').val()) : 0;
+
+        const feeTotal = mortgageTotal + transferFees;
+
+        return feeTotal;
+    }
+});
+
+
+// var doc = new jsPDF()
+
+// doc.text('Hello world!', 10, 10)
+// doc.save('a4.pdf')
